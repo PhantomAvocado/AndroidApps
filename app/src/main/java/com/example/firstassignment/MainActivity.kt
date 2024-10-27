26f2148047c9f2bf648c2b69fac6f67656157f77
PhantomAvocado <roma.kot1000@gmail.com>
2024-10-27 15:56:26 +0100

    First commit

--- app/src/main/java/com/example/firstassignment/MainActivity.kt
+++ app/src/main/java/com/example/firstassignment/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.firstassignment
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +16,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
+        val textstr= findViewById<TextView>(R.id.text_col)
+        val spanString=SpannableString(textstr.text)
+        val spanColour= ForegroundColorSpan(Color.RED)
+        spanString.setSpan(spanColour,12,23, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        textstr.text=spanString
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
     }
 }
