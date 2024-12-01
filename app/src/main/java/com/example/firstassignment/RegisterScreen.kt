cc95249be858305fea2270050e721e7ac4e47e2f
PhantomAvocado <roma.kot1000@gmail.com>
2024-12-01 18:39:29 +0100

    Fixed naming, removed registerscreen folder

--- app/src/main/java/com/example/firstassignment/RegisterScreen.kt
+++ app/src/main/java/com/example/firstassignment/RegisterScreen.kt
@@ -4,19 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+
+//given proper credentials, create an account
 
 class RegisterScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_screen)
 
+
         val logIn = findViewById<TextView>(R.id.logIn)
         logIn.setOnClickListener {
             Log.d("Homework","Pressing the 'log in' text")
 
-            val goToLogInIntent = Intent(this,MainActivity::class.java)
+            val goToLogInIntent = Intent(this,LogInActivity::class.java)
             startActivity(goToLogInIntent)
         }
     }
