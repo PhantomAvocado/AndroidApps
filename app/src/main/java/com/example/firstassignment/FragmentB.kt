675d3b93a7d6d0d80f38482b2123dde6e19aaa53
olive-mint <62811905+kefisan@users.noreply.github.com>
2024-12-11 13:12:35 +0100

    lyra

---
+++ app/src/main/java/com/example/firstassignment/FragmentB.kt
@@ -0,0 +1,19 @@
+package com.example.firstassignment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import android.widget.TextView
+
+class FragmentB : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_b, container, false)
+        view.findViewById<TextView>(R.id.textView).text = "FragmentB"
+        return view
+    }
+}
